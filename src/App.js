a81b8546b7b11ec5273dc60ac2765aee1f022704
Jean Carllo <jeanrosete04@gmail.com>
2024-11-19 17:09:22 -0300

    realizado o funcionamento do save com tds as 4 fotos.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,62 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+// import Malha from "./components/malha/index.jsx";
+// import Molliers from "./components/molliers/index.jsx";
+// import { GlobalCss } from "./styles.tsx";
+
+// function App() {
+
+//   const [salvarMalha, setSalvarMalha] = useState(false);
+//   const [salvarMolliers, setSalvarMolliers] = useState(false);
+
+//   const handleSalvarMalha = () => {
+//     setSalvarMalha(true);
+//   };
+
+//   const handleSalvarMolliers = () => {
+//     setSalvarMolliers(true);
+//   };
+  
+//   return (
+//     <>
+//     <GlobalCss />
+//     <div className="App">
+//     <Malha salvar={salvarMalha} setSalvar={setSalvarMalha} />
+//       <Molliers salvar={salvarMolliers} setSalvar={setSalvarMolliers} />
+//       <button onClick={handleSalvarMalha}>Salvar Malha</button>
+//       <button onClick={handleSalvarMolliers}>Salvar Molliers</button>
+//     </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState } from "react";
 
 import Malha from "./components/malha/index.jsx";
 import Molliers from "./components/molliers/index.jsx";
 import { GlobalCss } from "./styles.tsx";
 
-
 function App() {
-
   const [salvarMalha, setSalvarMalha] = useState(false);
   const [salvarMolliers, setSalvarMolliers] = useState(false);
 
-  const handleSalvarMalha = () => {
+  // Função que salva ambos os componentes
+  const handleSalvarTudo = () => {
     setSalvarMalha(true);
-  };
-
-  const handleSalvarMolliers = () => {
     setSalvarMolliers(true);
   };
-  
+
   return (
     <>
-    <GlobalCss />
-    <div className="App">
-    <Malha salvar={salvarMalha} setSalvar={setSalvarMalha} />
-      <Molliers salvar={salvarMolliers} setSalvar={setSalvarMolliers} />
-      <button onClick={handleSalvarMalha}>Salvar Malha</button>
-      <button onClick={handleSalvarMolliers}>Salvar Molliers</button>
-    </div>
+      <GlobalCss />
+      <div className="App">
+        <Malha salvar={salvarMalha} setSalvar={setSalvarMalha} />
+        <Molliers salvar={salvarMolliers} setSalvar={setSalvarMolliers} />
+        <button onClick={handleSalvarTudo}>Salvar Tudo</button>
+      </div>
     </>
   );
 }
